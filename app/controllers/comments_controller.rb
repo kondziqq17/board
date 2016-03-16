dc8b3809671a381b7078e14874e0ea738f0bbce2
Konrad Pason <konrad.pason17@gmail.com>
2016-03-16 14:02:00 +0100

    Added timestamps and allowed only users logied in to create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
+	before_action :find_message
+	before_action :find_comment, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!
 
 	def create
-		@message = Message.find(params[:message_id])
 		@comment = @message.comments.create(comment_params)
-		@comment.user_id = current_user_id
+		@comment.user_id = current_user.id
 
 		if @comment.save
 			redirect_to message_path(@message)
@@ -12,10 +14,37 @@ class CommentsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+
+	def update
+		if @comment.update(comment_params)
+			redirect_to message_path(@message)
+		else
+			render 'edit'
+		end
+	end
+
+		def destroy
+			@comment.destroy
+			redirect_to message_path(@message)
+		end
+
+
+
 	private
 
 		def comment_params
 			params.require(:comment).permit(:content)
 		end
 
-end
+		def find_message
+			@message = Message.find(params[:message_id])
+		end
+
+		def find_comment
+			@comment = @message.comments.find(params[:id])
+		end
+		
+	end
